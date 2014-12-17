8fb0c2a14fa1416a3dd500c7738b7db5b0dd6106
Steve Good <steve@stevegood.org>
2014-12-17 01:00:37 -0600

    Created the first bit of integration between the API and the UX application.  the user service can get users by their usernames.  Rejoice!

--- src/main/groovy/tv/extrememoderation/sec/UserRepository.groovy
+++ src/main/groovy/tv/extrememoderation/sec/UserRepository.groovy
@@ -1,11 +1,15 @@
 package tv.extrememoderation.sec
 
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.RequestParam
 
 /**
  * Created by Steve on 12/15/2014.
  */
 @Component
 interface UserRepository extends MongoRepository<User, String> {
+
+    User findByUsername(@Param('username') String username)
 }
