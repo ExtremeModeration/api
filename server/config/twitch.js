3b7a41aa1ccf125565cfce7ff389a1a8f28d064d
Steve Good <steve@stevegood.org>
2015-01-08 23:45:49 -0600

    Created a holder for twitch api credential thingies.

---
+++ server/config/twitch.js
@@ -0,0 +1,9 @@
+/**
+ * Created by steve on 1/8/15.
+ */
+module.exports = {
+    client: {
+        id: process.env.TWITCH_CLIENT_ID || 'osz9c2zqlvf7am0q20ex81yeisxectq',
+        secret: process.env.TWITCH_CLIENT_SECRET
+    }
+};
